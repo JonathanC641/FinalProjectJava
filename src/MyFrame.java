c292973c8f6e621bd0676f24ae6f83b96e973212
1e10Laptop <sample@bths.edu>
2023-05-31 10:56:29 -0400

    Code!

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -8,17 +8,15 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements MouseListener, KeyListener, MouseWheelListener{
 
 
     JLabel label;
     JLabel label2;
 
-    ImageIcon icon;
-    ImageIcon icon2;
+    ImageIcon billy;
+    ImageIcon timmy;
 
-
-    ImageIcon image;
 
     MyPanel panel;
 
@@ -27,12 +25,90 @@ public class MyFrame extends JFrame {
     MyFrame(){
         panel = new MyPanel();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(panel);
         this.setSize(1000,1000);
-
         this.setLayout(new FlowLayout());
+        //Characters
+        billy = new ImageIcon("C:\\Users\\student\\IdeaProjects\\FinalProjectJava\\src\\billy.png");
+        timmy = new ImageIcon("C:\\Users\\student\\IdeaProjects\\FinalProjectJava\\src\\pixil-frame-0.png");
+        label = new JLabel();
+        label2 = new JLabel();
+        label.setIcon(billy);
+        label2.setIcon(timmy);
+        this.add(label);
+        this.add(label2);
+        this.add(panel,BorderLayout.SOUTH);
+        addKeyListener(this);
+        addMouseListener(this);
+        addMouseWheelListener(this);
         this.setVisible(true);
     }
 
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Mouse Clicked!");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("Mouse Pressed!");
+        if(e.getButton() == 1){
+            label2.setLocation(label2.getX()+3,label2.getY()+10);
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("Mouse Released!");
+
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        int scroll = e.getWheelRotation();
+        if(scroll > 0){
+            label2.setLocation(label2.getX()-10, label2.getY());
+        }else{
+            label2.setLocation(label2.getX()+10, label2.getY());
+        }
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        switch(e.getKeyChar()) {
+            case 'a': label.setLocation(label.getX()-10, label.getY());
+                break;
+            case 'w': label.setLocation(label.getX(), label.getY()-10);
+                break;
+            case 's': label.setLocation(label.getX(), label.getY()+10);
+                break;
+            case 'd': label.setLocation(label.getX()+10, label.getY());
+                break;
+        }
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+   
 }
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,55 +1,21 @@
 import java.awt.*;
+
 import javax.swing.*;
 
-public class MyPanel extends JPanel implements KeyListener {
+public class MyPanel extends JPanel{
     final int PANEL_WIDTH = 1920;
     final int PANEL_HEIGHT = 1080;
 
-    final int BILLY_HEIGHT = 1100;
-    Image billy;
-    Image Jokic;
     Image background;
 
-    Timer timer;
-    int xVelocity = 5;
-    int yVelocity = 5;
-    int x = 0;
-    int y = 0;
 
 
 
 
     MyPanel(){
-
         background = new ImageIcon("C:\\Users\\student\\IdeaProjects\\FinalProjectJava\\src\\cliff.png").getImage();
         this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
-        billy = new ImageIcon("C:\\Users\\student\\IdeaProjects\\FinalProjectJava\\src\\billy.png").getImage();
-        Jokic = new ImageIcon("C:\\Users\\student\\IdeaProjects\\FinalProjectJava\\src\\pixil-frame-0.png").getImage();
-//        timer = new Timer(10, this);
-//        timer.start();
-    }
-
-    public void paint(Graphics g) {
-        super.paint(g);
-        Graphics2D g2D = (Graphics2D) g;
-        g2D.drawImage(background, 0, 0, this);
-        g2D.drawImage(Jokic,x,y,this);
-        g2D.drawImage(billy,x,y,this);
-    }
-
-
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
 
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
 }
